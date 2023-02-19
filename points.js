46bdb3ff68046f81e41ccba03dc827bb21505105
TonoCatMeow <86448220+TonoCatMeow@users.noreply.github.com>
2023-02-18 22:10:42 -0500

    Create points.js

---
+++ points.js
@@ -0,0 +1,9 @@
+function countClicks() {
+    var clicks = parseInt(localStorage.getItem('clicks')) || 0;
+    clicks++;
+    localStorage.setItem('clicks', clicks);
+    document.getElementById('clicks').innerHTML = clicks;
+
+    // navigate to another page with the number of clicks as a parameter
+    window.location.href = "otherpage.html?clicks=" + clicks;
+}
